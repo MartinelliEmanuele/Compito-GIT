805e604f387dae735d7e9f4f8917ab95bf3de732
Martinelli Emanuele <martinelli.emanuele@itismeucci.com>
2016-12-19 10:35:57 +0100

    renamed i into cont

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ Programma che esegue un ciclo da 1 a 10 e visualizza in output la variabile cont
 #include <stdio.h>
 using namespace std; 
 int main(){
-	for(int i=0; i<10; i++){
-		cout << i;
+	for(int cont=0; cont<10; cont++){
+		cout << cont;
 	}
 }
