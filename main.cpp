2284c3ed33dac20925edcc65ccefb153224e1e2c
Martinelli Emanuele <martinelli.emanuele@itismeucci.com>
2016-12-19 10:32:22 +0100

    added {} to the cycle

--- main.cpp
+++ main.cpp
@@ -4,8 +4,9 @@ Programma che esegue un ciclo da 1 a 10 e visualizza in output la variabile cont
 @Version 1.0 19/12/2016
 */
 #include <stdio.h>
-using namespace std;
+using namespace std; 
 int main(){
-	for(int i=0; i<10; i++)
+	for(int i=0; i<10; i++){
 		cout << i;
+	}
 }
