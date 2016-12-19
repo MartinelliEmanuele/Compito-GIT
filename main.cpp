5b69b8ebee639bb7a3c7a6208da009cc5befb3d4
Martinelli Emanuele <martinelli.emanuele@itismeucci.com>
2016-12-19 10:26:19 +0100

    First commit

---
+++ main.cpp
@@ -0,0 +1,11 @@
+/**
+Programma che esegue un ciclo da 1 a 10 e visualizza in output la variabile contatore
+@Author Emanuele Martinelli
+@Version 1.0 19/12/2016
+*/
+#include <stdio.h>
+using namespace std;
+int main(){
+	for(int i=0; i<10; i++)
+		cout << i;
+}
